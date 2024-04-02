d53cd95ff8a4640f163ace8c4bd06a98c860e84a
David Moraes <DavidMoraes-DEV@outlook.com>
2024-04-02 06:49:07 -0300

    Account Deposit Unit Tests

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -1,5 +1,38 @@
 package tests.entities;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Account;
+import tests.factory.AccountFactory;
+
 public class AccountTests {
+
+	@Test
+	public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
+		
+		double amount = 200;
+		double expectedValue = 196;
+		
+		Account acc = AccountFactory.createEmptyAccount();
+		
+		acc.deposit(amount);
+		
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+	
+	@Test
+	public void depositShouldDoNothingWhenNegativeAmount() {
+		
+		double expectedValue = 100.0;
+		Account acc = AccountFactory.createAccount(expectedValue);
+		
+		double amount = -200;
+		
+		acc.deposit(amount);
+		
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+		
+	}
 	
 }
